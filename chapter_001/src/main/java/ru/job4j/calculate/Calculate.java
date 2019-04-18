b421fb7e54f1ad0d15e931cb1f1885c913bdea9f
RobertArkhipov <s34s@bk.ru>
2019-04-18 23:16:42 +0300

    1.3. JavaDoc. Code convention[#133328]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,16 @@
 package ru.job4j.calculate;
 
+/**
+* Класс Calculate - выводи строку в консоль.
+* @author Robert Arkhipov (s34s@bk.ru)
+* @since 18.04.2019
+* @version 1
+*/
 public class Calculate {
-
+/**
+* Main.
+* @param args - args.
+*/
     public static void main(String[] args) {
 	    System.out.println("Hello World");
     }
