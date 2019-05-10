ed7bd904fe59759ee8ff86020e8a2379335e4fea
RobertArkhipov <s34s@bk.ru>
2019-05-10 23:20:51 +0300

    6.4. Слово начинается с ...

---
+++ chapter_001/src/test/java/ru/job4j/array/ArrayCharTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ */
+public class ArrayCharTest {
+
+    /**
+     * Если слово начинается с префикса, значит true.
+     */
+    @Test
+    public void whenStartWithPrefixThenTrue() {
+        ArrayChar word = new ArrayChar("Hello");
+        boolean result = word.startWith("He");
+        assertThat(result, is(true));
+    }
+
+    /**
+     * Если слово не начинается с префикса, значит false.
+     */
+    @Test
+    public void whenNotStartWithPrefixThenFalse() {
+        ArrayChar word = new ArrayChar("Hello");
+        boolean result = word.startWith("Hi");
+        assertThat(result, is(false));
+    }
+}
