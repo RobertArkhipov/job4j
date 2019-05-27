97cd2493010caa2c1ede8701ffedd30487d6ef9e
RobertArkhipov <s34s@bk.ru>
2019-05-27 23:48:53 +0300

    1. Рефакторинг - Расстояние между точками.[#133415]

--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -6,10 +6,38 @@ import static org.junit.Assert.assertThat;
 
 public class PointTest {
 
+    /**
+     * Тест сравнивает расстояние между точками и ожидаемое значение.
+     */
     @Test
     public void whenZeroAndTenThenTen() {
-        Point point = new Point();
-        double result = point.distance(0, 0, 0, 10);
+        Point first = new Point(0, 0);
+        Point second = new Point(0, 10);
+        double result = first.distance(second);
+        first.info();
+        second.info();
+        System.out.println(String.format("Result is %s", result));
         assertThat(result, is(10D));
+    }
+
+    /**
+     * Тест, который рассчитывает расстояния до себя.
+     */
+    @Test
+    public void whenCheckItself() {
+        Point point = new Point(0, 0);
+        double result = point.distance(point);
+        assertThat(result, is(0D));
+    }
+
+    /**
+     * Тест для просмотра вывода состояния объекта.
+     */
+    @Test
+    public void whenShowInfo() {
+        Point first = new Point(1, 1);
+        first.info();
+        Point second = new Point(2, 2);
+        second.info();
     }
 }
