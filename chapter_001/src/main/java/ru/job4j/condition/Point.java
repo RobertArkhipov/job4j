0eec64b2827a2c3f3408f373f22e44719dd45eec
RobertArkhipov <s34s@bk.ru>
2019-05-29 23:34:44 +0300

    3. Расстояние между точками в трехмерном пространстве.[#133417]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -7,12 +7,12 @@ import static java.lang.Math.sqrt;
  * Программа для определения расстояния между точками в системе координат.
  */
 public class Point {
-
 	private int x;
 	private int y;
+	private int z;
 
 	/**
-	 * Конструтор, который принимает начальное состояние объекта "точка".
+	 * Конструтор, который принимает начальное состояние объекта "точка" в двумерном пространстве.
 	 * @param first координата x.
 	 * @param second координата y.
 	 */
@@ -22,7 +22,18 @@ public class Point {
 	}
 
 	/**
-	 * Расстояние между точками в системе координат.
+	 * Конструтор, который принимает начальное состояние объекта "точка" в трёхмерном пространстве.
+	 * @param x координата x.
+	 * @param y координата y.
+	 * @param z координата z.
+	 */
+ 	public Point(int x, int y, int z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+ }
+	/**
+	 * Расстояние между точками в системе координат в двумерном пространстве.
 	 * @param that координаты второй точки.
 	 * @return расстояние.
 	 */
@@ -30,7 +41,20 @@ public class Point {
 		return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
 	}
 
+	/**
+	 * Расстояние между точками в системе координат в трёхмерном пространстве.
+	 * @param that координаты второй точки.
+	 * @return расстояние.
+	 */
+	public double distance3d(Point that) {
+		return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2) + pow(this.z - that.z, 2));
+	}
+
 	public void info() {
 		System.out.println(String.format("Point[%s, %s]", this.x, this.y));
+	}
+
+	public void info3d() {
+		System.out.println(String.format("Point[%s, %s, %s]", this.x, this.y, this.z));
 	}
 }
--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertThat;
 public class PointTest {
 
     /**
-     * Тест сравнивает расстояние между точками и ожидаемое значение.
+     * Тест сравнивает расстояние между точками в двумерном пространстве, и ожидаемое значение.
      */
     @Test
     public void whenZeroAndTenThenTen() {
@@ -21,7 +21,21 @@ public class PointTest {
     }
 
     /**
-     * Тест, который рассчитывает расстояния до себя.
+     * Тест сравнивает расстояние между точками в трёхмерном пространстве, и ожидаемое значение.
+     */
+    @Test
+    public void whenZeroTenAndZeroThenTen() {
+        Point first = new Point(0, 0, 0);
+        Point second = new Point(0, 10, 0);
+        double result = first.distance3d(second);
+        first.info3d();
+        second.info3d();
+        System.out.println(String.format("Result is %s", result));
+        assertThat(result, is(10D));
+    }
+
+    /**
+     * Тест, который рассчитывает расстояние до себя в двумерном пространстве.
      */
     @Test
     public void whenCheckItself() {
@@ -31,13 +45,34 @@ public class PointTest {
     }
 
     /**
-     * Тест для просмотра вывода состояния объекта.
+     * Тест, который рассчитывает расстояние до себя в трёхмерном пространстве.
+     */
+    @Test
+    public void whenCheckItself3d() {
+        Point point = new Point(0, 0, 0);
+        double result = point.distance3d(point);
+        assertThat(result, is(0D));
+    }
+
+    /**
+     * Тест для просмотра вывода состояния объекта в двумерном пространстве.
      */
     @Test
     public void whenShowInfo() {
         Point first = new Point(1, 1);
-        first.info();
+        first.info3d();
         Point second = new Point(2, 2);
         second.info();
+    }
+
+    /**
+     * Тест для просмотра вывода состояния объекта в трёхмерном пространстве.
+     */
+    @Test
+    public void whenShowInfo3d() {
+        Point first = new Point(1, 1, 0);
+        first.info3d();
+        Point second = new Point(2, 2, 3);
+        second.info3d();
     }
 }
