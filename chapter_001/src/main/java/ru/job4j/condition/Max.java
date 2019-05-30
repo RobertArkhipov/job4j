19d7606863a0d96f26668ce66845200d6dff7174
RobertArkhipov <s34s@bk.ru>
2019-05-30 22:56:42 +0300

    4. Перегрузить метод max для трех чисел[#133418]

--- chapter_001/src/main/java/ru/job4j/condition/Max.java
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -1,17 +1,42 @@
 package ru.job4j.condition;
 
 /**
- * Программа для нахождения максимума из двух чисел.
+ * Программа для нахождения максимума из нескольких чисел.
  */
 public class Max {
 
     /**
-     * Метод max возвращает максимальное число из left и right.
-     * @param left первое проверяемое число.
-     * @param right второе проверяемое число.
+     * Метод max возвращает максимальное число из first и second.
+     * @param first первое проверяемое число.
+     * @param second второе проверяемое число.
      * @return максимальное число.
      */
-    public int max(int left, int right) {
-        return left < right ? right : left;
+    public int max(int first, int second) {
+        return first < second ? second : first;
+    }
+
+    /**
+     * Метод max возвращает максимальное число из first, second и third.
+     * @param first первое проверяемое число.
+     * @param second второе проверяемое число.
+     * @param third третье проверяемое число.
+     * @return максимальное число.
+     */
+    public int max(int first, int second, int third) {
+        int tmp = max(second, third);
+        return max(first, tmp);
+    }
+
+    /**
+     * Метод max возвращает максимальное число из first, second, third и fourth.
+     * @param first первое проверяемое число.
+     * @param second второе проверяемое число.
+     * @param third третье проверяемое число.
+     * @param fourth четвёртое проверяемое число.
+     * @return максимальное число.
+     */
+    public int max(int first, int second, int third, int fourth) {
+        int tmp = max(second, third, fourth);
+        return max(first, tmp);
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -13,7 +13,7 @@ import static org.junit.Assert.assertThat;
 public class MaxTest {
 
     /**
-     * Тест-методы с проверками: второе число максимальное, первое число максимальное, оба числа одинаковые.
+     * Тест, при котором второе число максимальное (проверяются 2 числа).
      */
     @Test
     public void whenMax1To2Then2() {
@@ -22,6 +22,9 @@ public class MaxTest {
         assertThat(result, is(2));
     }
 
+    /**
+     * Тест, при котором первое число максимальное (проверяются 2 числа).
+     */
     @Test
     public void whenMax2To1Then2() {
         Max max = new Max();
@@ -29,10 +32,53 @@ public class MaxTest {
         assertThat(result, is(2));
     }
 
+    /**
+     * Тест, при котором первое число максимальное (проверяются 3 числа).
+     */
+    @Test
+    public void whenMax5Or1Or2Then5() {
+        Max max = new Max();
+        int result = max.max(5, 1, 2);
+        assertThat(result, is(5));
+    }
+
+    /**
+     * Тест, при котором третье число максимальное (проверяются 4 числа).
+     */
+    @Test
+    public void whenMax1Or4Or7Or5Then5() {
+        Max max = new Max();
+        int result = max.max(1, 4, 7, 5);
+        assertThat(result, is(7));
+    }
+
+    /**
+     * Тест, при котором оба числа одинаковые (проверяются 2 числа).
+     */
     @Test
     public void whenMax1To1Then1() {
         Max max = new Max();
         int result = max.max(1, 1);
         assertThat(result, is(1));
+    }
+
+    /**
+     * Тест, при котором все числа одинаковые (проверяются 3 числа).
+     */
+    @Test
+    public void whenMax1Or1Or1Then1() {
+        Max max = new Max();
+        int result = max.max(1, 1, 1);
+        assertThat(result, is(1));
+    }
+
+    /**
+     * Тест, при котором все числа одинаковые (проверяются 4 числа).
+     */
+    @Test
+    public void whenMax3Or3Or3Or3Then3() {
+        Max max = new Max();
+        int result = max.max(3, 3, 3, 3);
+        assertThat(result, is(3));
     }
 }
