14da51c67e77a024e100888003d576a30851d1bc
RobertArkhipov <s34s@bk.ru>
2019-07-13 23:57:55 +0300

    1. Используя класс ConsoleInput в классе StartUI обеспечить полноценную работу всего приложения[#133369]

---
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,88 @@
+package ru.job4j.tracker;
+
+/**
+ * @version $Id$
+ * @since 0.1
+ */
+public class StartUI {
+
+    /**
+     * Константа меню для добавления новой заявки.
+     */
+    private static final String ADD = "0";
+
+    /**
+     * Константа для выхода из цикла.
+     */
+    private static final String EXIT = "6";
+    /**
+     * Получение данных от пользователя.
+     */
+    private final Input input;
+
+    /**
+     * Хранилище заявок.
+     */
+    private final Tracker tracker;
+
+    /**
+     * Конструтор инициализирующий поля.
+     * @param input ввод данных.
+     * @param tracker хранилище заявок.
+     */
+    public StartUI(Input input, Tracker tracker) {
+        this.input = input;
+        this.tracker = tracker;
+    }
+
+    /**
+     * Основой цикл программы.
+     */
+    public void init() {
+        boolean exit = false;
+        while (!exit) {
+            this.showMenu();
+            String answer = this.input.ask("Введите пункт меню : ");
+            if (ADD.equals(answer)) {
+                //добавление заявки вынесено в отдельный метод.
+                this.createItem();
+//            } else if (...) {
+//             Добавить остальные действия системы по меню.
+            } else if (EXIT.equals(answer)) {
+                exit = true;
+            }
+        }
+    }
+
+    /**
+     * Метод реализует добавленяи новый заявки в хранилище.
+     */
+    private void createItem() {
+        System.out.println("------------ Добавление новой заявки --------------");
+        String name = this.input.ask("Введите имя заявки :");
+        String desc = this.input.ask("Введите описание заявки :");
+        Item item = new Item(name, desc, 0);
+        this.tracker.add(item);
+        System.out.println("------------ Новая заявка с getId : " + item.getId() + "-----------");
+    }
+
+    private void showMenu() {
+        System.out.println("Меню.");
+        System.out.println("0. Добавить новую заявку");
+        System.out.println("1. Отобразить все заявки");
+        System.out.println("2. Редактировать заявку");
+        System.out.println("3. Удалить заявку");
+        System.out.println("4. Поиск заявки по Id");
+        System.out.println("5. Поиск заявки по имени");
+        System.out.println("6. Выход из программы");
+        System.out.println("Выберите пункт меню:");
+    }
+
+    /**
+     * Запускт программы.
+     * @param args
+     */
+    public static void main(String[] args) {
+        new StartUI(new ConsoleInput(), new Tracker()).init();
+    }
+}
