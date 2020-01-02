177ed4bd35d6663a4c50c4d3dc406e984ab72092
RobertArkhipov <s34s@bk.ru>
2020-01-02 23:16:18 +0300

    2. Сортировка User с использованием Comparator[#133347] 2.0

--- chapter_002/src/test/java/ru/job4j/collection/JobTest.java
+++ chapter_002/src/test/java/ru/job4j/collection/JobTest.java
@@ -2,6 +2,7 @@ package ru.job4j.collection;
 
 import org.junit.Test;
 import java.util.Comparator;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.lessThan;
 import static org.junit.Assert.assertThat;
 
@@ -9,12 +10,61 @@ import static org.junit.Assert.assertThat;
  * Test.
  */
 public class JobTest {
+
+    /**
+     * Тест-метод отправляет 2 параметра в метод compare класса JobDescByName,
+     * и сравнивает с ожидаемым результатом. Ожидается корректное
+     * сравнение полей имени (для сортировки по убыванию).
+     */
     @Test
-    public void whenCompatorByNameAndPrority() {
+    public void whenComparatorByName() {
+        Comparator<Job> cmpName = new JobDescByName();
+        int rsl = cmpName.compare(
+                new Job("Task1", 0),
+                new Job("Task2", 0)
+        );
+        assertThat(rsl, is(1));
+    }
+
+    /**
+     * Тест-метод отправляет 2 параметра в метод compare класса JobDescByPriority,
+     * и сравнивает с ожидаемым результатом. Ожидается корректное
+     * сравнение полей приоритета (для сортировки по убыванию).
+     */
+    @Test
+    public void whenComparatorByPriority() {
+        Comparator<Job> cmpPriority = new JobDescByPriority();
+        int rsl = cmpPriority.compare(
+                new Job("Task1", 1),
+                new Job("Task2", 0)
+        );
+        assertThat(rsl, lessThan(0));
+    }
+
+    /**
+     * Тест-метод проверяет комбинированную сортировку
+     * по полям имени и приоритета (по убыванию).
+     */
+    @Test
+    public void whenComparatorByNameAndPriority() {
         Comparator<Job> cmpNamePriority = new JobDescByName().thenComparing(new JobDescByPriority());
         int rsl = cmpNamePriority.compare(
                 new Job("Impl task", 0),
                 new Job("Fix bug", 1)
+        );
+        assertThat(rsl, lessThan(0));
+    }
+
+    /**
+     * Тест-метод проверяет комбинированную сортировку
+     * по полям приоритета и имени (по убыванию).
+     */
+    @Test
+    public void whenComparatorByPriorityAndName() {
+        Comparator<Job> cmpPriorityName = new JobDescByPriority().thenComparing(new JobDescByName());
+        int rsl = cmpPriorityName.compare(
+                new Job("Task1", 1),
+                new Job("Task2", 0)
         );
         assertThat(rsl, lessThan(0));
     }
--- chapter_002/src/test/java/ru/job4j/collection/UserComparatorTest.java
+++ chapter_002/src/test/java/ru/job4j/collection/UserComparatorTest.java
@@ -2,14 +2,75 @@ package ru.job4j.collection;
 
 import org.junit.Test;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 
 /**
  * Test.
  */
 public class UserComparatorTest {
+
+    /**
+     * Тест-метод отправляет 2 параметра в метод compare класса UserAscByAge,
+     * и сравнивает с ожидаемым результатом. Ожидается корректное
+     * сравнение полей возраста (для сортировки по возрастанию).
+     */
+    @Test
+    public void whenAscByAge() {
+        Comparator<User> ascByAge = new UserAscByAge();
+        int result = ascByAge.compare(
+                new User("Petr", 21),
+                new User("Ivan", 24)
+        );
+        assertThat(result, lessThan(0));
+    }
+
+    /**
+     * Тест-метод отправляет 2 параметра в метод compare класса UserDescByAge,
+     * и сравнивает с ожидаемым результатом. Ожидается корректное
+     * сравнение полей возраста (для сортировки по убыванию).
+     */
+    @Test
+    public void whenDescByAge() {
+        Comparator<User> descByAge = new UserDescByAge();
+        int result = descByAge.compare(
+                new User("Petr", 21),
+                new User("Ivan", 24)
+        );
+        assertThat(result, is(1));
+    }
+
+    /**
+     * Тест-метод отправляет 2 параметра в метод compare класса UserAscByName,
+     * и сравнивает с ожидаемым результатом. Ожидается корректное
+     * сравнение полей имени (для сортировки по возрастанию).
+     */
+    @Test
+    public void whenAscByName() {
+        Comparator<User> ascByName = new UserAscByName();
+        int result = ascByName.compare(
+                new User("Ivan", 24),
+                new User("Petr", 21)
+        );
+        assertThat(result, lessThan(0));
+    }
+
+    /**
+     * Тест-метод отправляет 2 параметра в метод compare класса UserDescByName,
+     * и сравнивает с ожидаемым результатом. Ожидается корректное
+     * сравнение полей имени (для сортировки по убыванию).
+     */
+    @Test
+    public void whenDescByName() {
+        Comparator<User> descByName = new UserDescByName();
+        int result = descByName.compare(
+                new User("Ivan", 24),
+                new User("Petr", 21)
+        );
+        assertThat(result, greaterThan(0));
+    }
 
     /**
      * Тест-метод добавляет List пользователей в метод nameAscending,
