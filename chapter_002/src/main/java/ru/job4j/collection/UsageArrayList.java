2ebee198edc10a389a8ac57dfbae9dcc8b365e5a
RobertArkhipov <s34s@bk.ru>
2020-01-07 19:29:25 +0300

    0. Коллекции, ArrayList, List, Обобщения [#10094]

---
+++ chapter_002/src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -0,0 +1,19 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+
+/**
+ * Task.
+ */
+public class UsageArrayList {
+
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Petr");
+        names.add("Ivan");
+        names.add("Stepan");
+        for (Object name: names) {
+            System.out.println(name);
+        }
+    }
+}
