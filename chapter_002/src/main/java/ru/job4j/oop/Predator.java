0fb1aa50d83406a15f4912c9cc709d7f6081a919
RobertArkhipov <s34s@bk.ru>
2020-02-18 23:09:50 +0300

    1. Конструктор по умолчанию. [#173889]

---
+++ chapter_002/src/main/java/ru/job4j/oop/Predator.java
@@ -0,0 +1,13 @@
+package ru.job4j.oop;
+
+public class Predator extends Animal {
+
+    public Predator() {
+        super();
+        System.out.println("load Predator");
+    }
+
+    public static void main(String[] args) {
+        Predator predator = new Predator();
+    }
+}
