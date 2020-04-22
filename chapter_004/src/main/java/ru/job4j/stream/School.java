4dea3de80278d906ab84d234f905f62c7e34bbd9
RobertArkhipov <s34s@bk.ru>
2020-04-22 21:20:00 +0300

    1. Фильтрация учеников. [#110058] 2.0

--- chapter_004/src/main/java/ru/job4j/stream/School.java
+++ chapter_004/src/main/java/ru/job4j/stream/School.java
@@ -1,8 +1,8 @@
 package ru.job4j.stream;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 /**
  * Task.
@@ -10,18 +10,7 @@ import java.util.function.Predicate;
 public class School {
 
     public static List<Student> collect(List<Student> students, Predicate<Student> predict) {
-        List<Student> rsl = new ArrayList<>();
-        for (Student value : students) {
-            if (predict.test(value)) {
-                rsl.add(value);
-            }
-        }
-        return rsl;
+        return students.stream().filter(
+                predict).collect(Collectors.toList());
     }
-    Predicate<Student> predict = new Predicate<Student>() {
-        @Override
-        public boolean test(Student student) {
-            return false;
-        }
-    };
 }
--- chapter_004/src/test/java/ru/job4j/stream/SchoolTest.java
+++ chapter_004/src/test/java/ru/job4j/stream/SchoolTest.java
@@ -2,72 +2,56 @@ package ru.job4j.stream;
 
 import org.junit.Test;
 import java.util.List;
-import java.util.stream.Collectors;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
- * Test. Проверяем фильтрацию учеников с помощью лямбда, а также лямбда совместно со Stream API.
+ * Test.
  */
 public class SchoolTest {
 
     /**
-     * В итоговый список учеников 10А класса должны попасть те, кто набрал больше 70 баллов.
+     * Тест-метод сравнивает ответ метода School.collect и ожидаемый результат.
+     * В ожидаемый список учеников 10А класса должны попасть те, кто набрал [70: 100] баллов.
      */
     @Test
     public void whenGrade10A() {
         List<Student> students = List.of(
                 new Student(100),
                 new Student(25),
-                new Student(73),
-                new Student(55),
-                new Student(2),
-                new Student(49)
+                new Student(55)
         );
-        List<Student> result = School.collect(students, student -> student.getScore() > 70);
-        List<Student> expected = students.stream().filter(
-                student -> student.getScore() > 70  && student.getScore() < 101
-        ).collect(Collectors.toList());
-        assertThat(result, is(expected));
+        List<Student> result = School.collect(students, student -> student.getScore() >= 70 && student.getScore() <= 100);
+        assertThat(result.get(0).getScore(), is(students.get(0).getScore()));
     }
 
     /**
-     * В итоговый список учеников 10Б класса должны попасть те, кто набрал больше 50 баллов, но меньше 71.
+     * Тест-метод сравнивает ответ метода School.collect и ожидаемый результат.
+     * В ожидаемый список учеников 10Б класса должны попасть те, кто набрал [50: 70) баллов.
      */
     @Test
     public void whenGrade10B() {
         List<Student> students = List.of(
                 new Student(100),
                 new Student(25),
-                new Student(73),
-                new Student(55),
-                new Student(2),
-                new Student(49)
+                new Student(55)
         );
-        List<Student> result = School.collect(students, student -> student.getScore() > 50 && student.getScore() < 71);
-        List<Student> expected = students.stream().filter(
-                student -> student.getScore() > 50 && student.getScore() < 71
-        ).collect(Collectors.toList());
-        assertThat(result, is(expected));
+        List<Student> result = School.collect(students, student -> student.getScore() >= 50 && student.getScore() < 70);
+        assertThat(result.get(0).getScore(), is(students.get(2).getScore()));
     }
 
     /**
-     * В итоговый список учеников 10В класса должны попасть те, кто набрал от 0 до 50 баллов(включительно)
+     * Тест-метод сравнивает ответ метода School.collect и ожидаемый результат.
+     * В ожидаемый список учеников 10В класса должны попасть те, кто набрал (0: 50) баллов.
      */
     @Test
     public void whenGrade10V() {
         List<Student> students = List.of(
                 new Student(100),
                 new Student(25),
-                new Student(73),
-                new Student(55),
-                new Student(2),
-                new Student(49)
+                new Student(55)
         );
-        List<Student> result = School.collect(students, student -> student.getScore() < 51);
-        List<Student> expected = students.stream().filter(
-                student -> student.getScore() >= 0 && student.getScore() < 51
-        ).collect(Collectors.toList());
-        assertThat(result, is(expected));
+        List<Student> result = School.collect(students, student -> student.getScore() > 0 && student.getScore() < 50);
+        assertThat(result.get(0).getScore(), is(students.get(1).getScore()));
     }
 }
