755fe36f1a4cc0d78d87a70d672c4246e179ca10
RobertArkhipov <s34s@bk.ru>
2020-05-25 23:23:48 +0300

    6.7.2. Моно столбец в матрице. [#214127]

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheckMono.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheckMono.java
@@ -22,4 +22,21 @@ public class MatrixCheckMono {
         }
         return result;
     }
+
+    /**
+     * Метод проверяет, что колонка в двухмерном массиве целиком заполнена символом 'X'.
+     * @param board двухмерный массив символов.
+     * @param column колонка, которую необходимо проверить.
+     * @return true or false.
+     */
+    public static boolean monoVertical(char[][] board, int column) {
+        boolean result = true;
+        for (int index = 0; index < board.length; index++) {
+            if (board[index][column] != 'X') {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
 }
